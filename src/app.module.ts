ad95b699bff47f6749898a991265ecafb770f12b
Zoe-997 <thuminh10011997@gmail.com>
2023-10-30 11:02:06 +0700

    Kết Nối Database Sử Dụng TypeORM (MySQL)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
+import { DataSource } from 'typeorm';
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/products/product.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProductModule, UsersModule],
+  imports: [
+    DatabaseModule,
+    ProductModule, 
+    UsersModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+  constructor (private datasource: DataSource) {}
+}
